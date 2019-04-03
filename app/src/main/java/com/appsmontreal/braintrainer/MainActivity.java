1e99278a85d77f9056321855e57dbc796b1db1e9
Marlon <escobar.marlon@gmail.com>
2019-04-03 17:20:42 -0400

    gameActivity

--- app/src/main/java/com/appsmontreal/braintrainer/MainActivity.java
+++ app/src/main/java/com/appsmontreal/braintrainer/MainActivity.java
@@ -1,13 +1,37 @@
 package com.appsmontreal.braintrainer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Intent intent;
+    Button buttonG0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialize();
     }
+
+    private void initialize() {
+         buttonG0 = findViewById(R.id.buttonGo);
+         buttonG0.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.buttonGo:
+                intent = new Intent(this,GameActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+
 }
